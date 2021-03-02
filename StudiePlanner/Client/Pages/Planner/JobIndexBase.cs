2722fe050d4e518e4819d4b0fefa41588ddd9799
Mathijs van de Kerkhof <mathijs.vandekerkhof@itvitaelearning.nl>
2021-03-02 14:14:05 +0100

    humpf

--- StudiePlanner/Client/Pages/Planner/JobIndexBase.cs
+++ StudiePlanner/Client/Pages/Planner/JobIndexBase.cs
@@ -14,10 +14,27 @@ namespace StudiePlanner.Client.Pages.Planner
         public IJobDataService JobDataService { get; set; }
         public List<Job> Jobs { get; set; }
 
+        protected string Message = string.Empty;
+        protected string StatusClass = string.Empty;
+        protected bool Saved;
+
         protected override async Task OnInitializedAsync()
         {
             Jobs = (await JobDataService.GetAllJobs()).ToList();
 
+        }
+
+        protected async Task CheckAllDates()
+        {
+            foreach (var job in Jobs)
+            {
+                if(job.EndDate <= DateTime.Now)
+                {
+                    StatusClass = "alert-danger";
+                    //Message = ($"{0} is nog niet afgerond: De einddatum is {1}", job.Name, job.EndDate.());
+                    Saved = true;
+                }
+            }
         }
     }
 }
